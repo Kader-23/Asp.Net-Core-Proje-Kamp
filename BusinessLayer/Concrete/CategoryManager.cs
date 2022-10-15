ce1d25e3e3d04302236bf3c84dcc1a7682cdcd00
Kader-23 <65540087+Kader-23@users.noreply.github.com>
2022-10-15 12:29:12 +0300

    Asp.Net-Core-Proje-Kamp
    Görev 1

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository efCtategoryRepository;
+        public CategoryManager()
+        {
+            efCtategoryRepository = new EfCategoryRepository();
+        }
+
+         public void CategoryAdd(Category category)
+        {
+            efCtategoryRepository.Insert(category);
+        }
+
+        public void CategoryDelete(Category category)
+        {
+
+            efCtategoryRepository.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            efCtategoryRepository.Update(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return efCtategoryRepository.GetById(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return efCtategoryRepository.GetListAll();
+        }
+    }
+}
